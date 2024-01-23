e4eb24af4b6f9b2c7e88d2afea8626e09e3d1b06
bumblingbe <79747651+bumblingbe@users.noreply.github.com>
2024-01-23 14:14:40 +0800

    Set up for Weather API

--- src/script.js
+++ src/script.js
@@ -8,5 +8,16 @@ function replaceOldDataWithSearchedForData(event) {
   cityNameElement.innerHTML = inputValueTrimmed;
 }
 
+function searchForCity(event) {
+  event.preventDefault();
+  let apiKey = "0bbe201oc9fead40c3t0cc4e349c3f4c";
+  let citySearched = document.querySelector("#city-search-bar");
+  let citySearchedValue = citySearched.value;
+  let citySearchedLowerTrimmed = citySearchedValue.trim().toLowerCase();
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${citySearchedLowerTrimmed}&key=${apiKey}`;
+}
+
 let submitButton = document.querySelector("#city-search-form");
 submitButton.addEventListener("submit", replaceOldDataWithSearchedForData);
+submitButton.addEventListener("submit", searchForCity);
