7457882b185859ede9fd67afaf1a721f1f103a8a
bumblingbe <79747651+bumblingbe@users.noreply.github.com>
2024-01-23 16:52:55 +0800

    Update all conditions & city with data from search input

--- src/script.js
+++ src/script.js
@@ -8,14 +8,68 @@ function replaceOldDataWithSearchedForData(event) {
   cityNameElement.innerHTML = inputValueTrimmed;
 }
 
+function askWhichUnits() {
+  let units = prompt("Units: metric or imperial?");
+  units = units.trim().toLowerCase();
+  if (units != "metric" && units != "imperial") {
+    alert(`Please enter either 'metric' or 'imperial'.`);
+    askWhichUnits();
+  } else {
+    return units;
+  }
+}
+
+function updateCityName(response) {
+  let cityNameElement = document.querySelector("#city-name");
+  let city = response.data.city;
+  cityNameElement.innerHTML = city;
+}
+
+function updateTodayWeather(response) {
+  //update temperature icon
+  let tempIconElement = document.querySelector("#temp-icon");
+  let tempIcon = response.data.condition.icon;
+  tempIconElement.innerHTML = tempIcon;
+
+  //update temperature
+  let tempValueElement = document.querySelector("#temp-value");
+  let tempValue = response.data.temperature.current;
+  tempValueElement.innerHTML = tempValue;
+
+  //update weather description
+  let weatherConditionDescriptionElement = document.querySelector(
+    "#weather-description"
+  );
+  let weatherConditionDescription = response.data.condition.description;
+  weatherConditionDescriptionElement.innerHTML = weatherConditionDescription;
+
+  //update humidity
+  let humidityValueElement = document.querySelector("#humidity-value");
+  let humidityValue = response.data.temperature.humidity;
+  humidityValueElement.innerHTML = humidityValue;
+
+  //update wind speed
+  let windSpeedElement = document.querySelector("#windspeed-value");
+  let windSpeed = response.data.wind.speed;
+  windSpeedElement.innerHTML = windSpeed;
+}
+
+function updateData(response) {
+  console.log(response.data);
+  updateCityName(response);
+  updateTodayWeather(response);
+}
+
 function searchForCity(event) {
   event.preventDefault();
   let apiKey = "0bbe201oc9fead40c3t0cc4e349c3f4c";
   let citySearched = document.querySelector("#city-search-bar");
   let citySearchedValue = citySearched.value;
   let citySearchedLowerTrimmed = citySearchedValue.trim().toLowerCase();
+  let units = askWhichUnits();
 
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${citySearchedLowerTrimmed}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${citySearchedLowerTrimmed}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(updateData);
 }
 
 let submitButton = document.querySelector("#city-search-form");
